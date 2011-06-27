ac77ea31ff3d7d7a206341686b2f666fe2d9962d
yesi <yesi.intasari@jerbeeindonesia.com>
2011-06-27 15:25:21 +0700

    template berita acara reduksinya

---
+++ billing/form_ba_reduksi.php
@@ -0,0 +1,100 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>Berita Acara Reduksi Rekening</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+<!--
+.style1 {font-size: 12px}
+-->
+</style>
+</head>
+<body >
+			
+ 			
+				<table width="100%" border="0" align="center" bgcolor="#FFFFFF">				
+				<tr class="form_cell">
+				  <td class="center"><div align="left"><img src="images/logorekeningkecil.gif" width="150" height="91"><h2> <span class="prepend-23 prepend-top">PDAM KABUPATEN BANDUNG</span></h2></div></td>
+				  </tr>
+				<tr class="form_cell">
+				<td class="center"><h1 align="center"><b> Berita Acara Reduksi Rekening </b></h1>
+				  <hr> </td>
+				</tr>				 				 
+				<tr> 
+					<td >
+				    <table width="70%">
+				    		<tr class="form_cell">
+				    		<td width="34%" class="table_cell1 style1" > No. Pelanggan</td> 
+				    		<td width="66%" class="table_cell1">:</td>                           			
+				    		<tr class="form_cell"><td class="table_cell1 style1" >Nama</td>	    
+				    		<td class="table_cell1"> : </td>        	                
+				    		<tr class="form_cell"><td class="table_cell1 style1" >Bulan-Tahun</td>   
+				    		<td class="table_cell1"> : 	</td>				
+				    		<tr class="form_cell">
+				    		<td class="table_cell1 style1" >Tanggal</td>           
+				    		<td class="table_cell1"> :	</td>      
+						</tr>             
+				   </table>	
+				   
+			<table width="100%" > 	 
+<tr class="table_head" > 
+ 
+				    <td colspan="2" class="center" >Sebelumnya</td>
+				    <td colspan="3" class="center">Hasil Koreksi</td>
+				    <td colspan="2" class="center" >Selisih</td>
+			  </tr>	 
+				  <tr class="table_head"> 				  				    
+				    <td class="center">Uang Air	</td>
+				    <td class="center">Nilai Total 	</td>
+				    <td class="center">Reduksi (%)    </td>
+				    <td class="center">Uang Air	</td>
+				    <td class="center">Nilai Total 	</td>
+				    <td class="center">Uang Air	</td>
+				    <td class="center">Nilai Total 	</td>
+				  </tr>
+						<tr valign="top" class="table_cell1" >
+						  <td align="right" >1</td>
+						  <td align="right" >1</td>
+						  <td align="right" >1</td>
+						  <td align="right" >1</td>
+						  <td align="right" >1</td>
+						  <td align="right" >1</td>
+						  <td align="right" >1</td>
+			  </tr>
+						<tr valign="top" class="table_cell1" >  
+ 
+				   		    <td align="right" >1234567</td>
+				   		    <td align="right" >123456</td>
+				   		    <td align="right" >1233 </td>   		    
+				     		<td align="right" >12344</td>
+				  		    <td align="right" >13344</td>
+				   		    <td align="right" >123333</td>
+				   		    <td align="right" >jwjeje</td>   		    				     		   
+	          </tr>	
+				  </table>				</tr>
+
+	 
+				<tr class="form_cell">
+					<td align="left"></td>
+				</tr>
+				
+				<tr class="form_cell">
+				<td height="204" align="left">
+				
+					<table width="80%" align="center">
+					<tr  class="form_cell">
+					<td  class="table_cell1 left">Diterima Konsumen <br/> 
+					  Tanggal : </td>
+					<td align="Left" class="table_cell1"><b>Kepala Cabang </b></td>
+	 				</tr>
+					<tr>
+					<td align="center" class="table_cell1 center"><br/>
+					  <br/><br/><br/><b>(_______________________)</b></td>
+					<td align="center" class="table_cell1 center"><br/>
+					  <br/><br/><br/><b>(_______________________)</b></td>
+	 				</tr>
+				  </table>			
+				</tr>
+ 			</table>
+</body>
+</html>
