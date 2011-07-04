436c07c1d66cf0733f2d159f929048110fd6c9a9
yesi <yesi.intasari@jerbeeindonesia.com>
2011-07-04 11:45:56 +0700

    lagi

--- billing/form_reduksi.php
+++ billing/form_reduksi.php
@@ -3,120 +3,121 @@
 	if(!isset($appl_tokn)) define("_TOKN",getToken());
 	switch($proses){
 		case "periksaDSR":
-			echo "Haaman Periksa DSR";
-			echo"<fieldset class=''>";
-			echo"<p><strong>REDUKSI REKENING </strong></p>";
-			echo"<table width='82%' >";
-				    		echo"<tr class='table_cell1'>
-				    		  <td width='18%' class='table_cell1' > No. Pelanggan</td> 
-				    		  <td width='21%' class='text'>:</td>
-				    		  <td width='25%' class='text'>&nbsp;</td>
-				    		  <td width='36%' class='text'>&nbsp;</td>";                           			
-  echo"<tr class='table_cell1'>
-    <td class='text' >Nama</td>	    
-    <td class='text'>:</td>
-    <td class='text'>Golongan</td>
-    <td class='text'> : 	</td>";        	                
-  echo"<tr class='table_cell1'>
-				    		  <td class='text' >Alamat</td>   
-    <td class='text'>:</td>
-    <td class='text'>Rayon Pembacaan </td>
-    <td class='text'> : </td>";				
-echo"</table>";	
-echo"<p><strong>REDUKSI SEBELUMNYA</strong></p>";
-echo"<table width='100%' >";
-				echo"<tr class='table_validator' > 
-				    <td rowspan='1'><div align='center'><strong>Tanggal</strong></div></td>				
+			echo "Halaman Periksa DSR";
+?>
+		<fieldset class="">
+			<p><strong>REDUKSI REKENING </strong></p>
+			<table width="82%" >
+				    		<tr class="table_cell1">
+				    		  <td width="18%" class="table_cell1" > No. Pelanggan</td> 
+				    		  <td width="21%" class="text">:</td>
+				    		  <td width="25%" class="text">&nbsp;</td>
+				    		  <td width="36%" class="text">&nbsp;</td>                           			
+  <tr class="table_cell1">
+    <td class="text" >Nama</td>	    
+    <td class="text">:</td>
+    <td class="text">Golongan</td>
+    <td class="text"> : 	</td>        	                
+  <tr class="table_cell1">
+				    		  <td class="text" >Alamat</td>   
+    <td class="text">:</td>
+    <td class="text">Rayon Pembacaan </td>
+    <td class="text"> : </td>				
+</table>	
+<p><strong>REDUKSI SEBELUMNYA</strong></p>
+<table width="100%" >
+				<tr class="table_validator" > 
+				    <td rowspan="1"><div align="center"><strong>Tanggal</strong></div></td>				
 				   
-				    <td colspan='2'><div align='center'><strong>Sebelumnya</strong></div></td>
-				    <td colspan='3' ><div align='center'><strong>Hasil Koreksi</strong></div></td>
-				    <td colspan='2' ><div align='center'><strong>Selisih</strong></div></td>
- 					 </tr>";
-				  echo"<tr class='table_validator'> 			
-				    <td><div align='center'> </div></td>	  				    
-				    <td><div align='center'><strong>Uang Air	</strong></div></td>
-				    <td><div align='center'><strong>Nilai Total 	</strong></div></td>
-				    <td><div align='center'><strong>Reduksi (%)    </strong></div></td>
-				    <td><div align='center'><strong>Uang Air	</strong></div></td>
-				    <td><div align='center'><strong>Nilai Total 	</strong></div></td>
-				    <td><div align='center'><strong>Uang Air	</strong></div></td>
-				    <td><div align='center'><strong>Nilai Total 	</strong></div></td>
-  					</tr>";
+				    <td colspan="2"><div align="center"><strong>Sebelumnya</strong></div></td>
+				    <td colspan="3" ><div align="center"><strong>Hasil Koreksi</strong></div></td>
+				    <td colspan="2" ><div align="center"><strong>Selisih</strong></div></td>
+ 					 </tr>
+				  <tr class="table_validator"> 			
+				    <td><div align="center"> </div></td>	  				    
+				    <td><div align="center"><strong>Uang Air	</strong></div></td>
+				    <td><div align="center"><strong>Nilai Total 	</strong></div></td>
+				    <td><div align="center"><strong>Reduksi (%)    </strong></div></td>
+				    <td><div align="center"><strong>Uang Air	</strong></div></td>
+				    <td><div align="center"><strong>Nilai Total 	</strong></div></td>
+				    <td><div align="center"><strong>Uang Air	</strong></div></td>
+				    <td><div align="center"><strong>Nilai Total 	</strong></div></td>
+  					</tr>
 
-						echo"<tr valign='top' class='table_cell1' >  
+						<tr valign="top" class="table_cell1" >  
 					 	    <td >&nbsp;</td>
 					 	    											 	    					
 					 	    <td >&nbsp;</td>
-				   		    <td align='right' >&nbsp;</td>
-				   		    <td align='right' >&nbsp;</td>
-				   		    <td align='right' >&nbsp;</td>   		    
-				     		<td align='right' >&nbsp;</td>
-				  		    <td align='right' >&nbsp;</td>
-				   		    <td align='right' >&nbsp;</td>
-                       </tr>";
+				   		    <td align="right" >&nbsp;</td>
+				   		    <td align="right" >&nbsp;</td>
+				   		    <td align="right" >&nbsp;</td>   		    
+				     		<td align="right" >&nbsp;</td>
+				  		    <td align="right" >&nbsp;</td>
+				   		    <td align="right" >&nbsp;</td>
+                       </tr>
 
-			echo"<tr class='table_validator'>
+			<tr class="table_validator">
 				<td>&nbsp;</td>
-				<td colspan='8'></td>
-  </tr>";
-echo"</table>";
-echo"<p class='style1'>REDUKSI </p>";
-echo"<table width='95%' border='1' >";
-  echo"<tr bgcolor='#02153F' class='table_validator'>
-    <td width='14%'><div align='center' class='style3'>No</div></td>
-    <td width='13%'><div align='center' class='style6 style3'>Bulan / Tahun </div></td>
-    <td colspan='2'><div align='center' class='style3'><span class='style6'>Sebelumnya</span></div></td>
-    <td colspan='2'><div align='center' class='style3'><span class='style6'>Sekarang (Reduksi) </span></div></td>
-    <td colspan='2'><div align='center' class='style3'><span class='style6'>Selisih</span></div></td>
-  </tr>";
-  echo"<tr>
-    <td rowspan='5' class='table_cell1 center'>123456789011212</td>
-    <td rowspan='5' class='table_cell1 center'>Juni 2011 </td>
-    <td width='13%' class='table_cell1'>Stan Lalu </td>
-    <td width='9%' class='table_cell1'>:</td>
-    <td colspan='2' rowspan='3' class='table_cell1'><form id='form1' name='form1' method='post' action=''>";
-      echo"<p>Reduksi
-        <input name='persen' type='text' id='persen' size='10' />
-        Persen </p>";
-        echo"<p align='center'>
-          <input type='button' name='Button' value='Hitung' class='hitung_button'/>   
-        </p>";
-    echo"</form></td>
-    <td width='13%' rowspan='3' class='table_cell1'>&nbsp;</td>
-    <td width='10%' rowspan='3' class='table_cell1'>&nbsp;</td>
-  </tr>";
-  echo"<tr>
-    <td height='27' class='table_cell1'>Stan Kini</td>
-    <td class='table_cell1'>:</td>
-  </tr>";
-  echo"<tr>
-    <td height='27' class='table_cell1'>Pemakaian</td>
-    <td class='table_cell1'>:</td>
-  </tr>";
-  echo"<tr>
-    <td height='34' class='table_cell1'>Uang Air</td>
-    <td class='table_cell1'>:</td>
-    <td width='16%' class='table_cell1'>Uang Air &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;:</td>
-    <td width='12%' class='table_cell1 right'>&nbsp;</td>
-    <td class='table_cell1'>Uang Air &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;:</td>
-    <td class='table_cell1 right'>&nbsp;</td>
-  </tr>";
-  echo"<tr>
-    <td height='27' class='table_cell1'>NILAI TOTAL </td>
-    <td class='table_cell1'>:</td>
-    <td class='table_cell1'>NILAI TOTAL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</td>
-    <td class='table_cell1 right'>&nbsp;</td>
-    <td class='table_cell1'>NILAI TOTAL &nbsp;&nbsp;&nbsp;&nbsp;:</td>
-    <td class='table_cell1 right'>&nbsp;</td>
-  </tr>";
-  echo"<tr bgcolor='#02153F' class='table_validator'>
-    <td height='30' colspan='8'><div align='right'><input name='Submit' type='submit' value='Reduksi' />
-       <input name='Submit2' type='submit' value='Batal' />
+				<td colspan="8"></td>
+  </tr>
+</table>
+<p class="style1">REDUKSI </p>
+<table width="95%" border="1" >
+  <tr bgcolor="#02153F" class="table_validator">
+    <td width="14%"><div align="center" class="style3">No</div></td>
+    <td width="13%"><div align="center" class="style6 style3">Bulan / Tahun </div></td>
+    <td colspan="2"><div align="center" class="style3"><span class="style6">Sebelumnya</span></div></td>
+    <td colspan="2"><div align="center" class="style3"><span class="style6">Sekarang (Reduksi) </span></div></td>
+    <td colspan="2"><div align="center" class="style3"><span class="style6">Selisih</span></div></td>
+  </tr>
+  <tr>
+    <td rowspan="5" class="table_cell1 center">123456789011212</td>
+    <td rowspan="5" class="table_cell1 center">Juni 2011 </td>
+    <td width="13%" class="table_cell1">Stan Lalu </td>
+    <td width="9%" class="table_cell1">:</td>
+    <td colspan="2" rowspan="3" class="table_cell1"><form id="form1" name="form1" method="post" action="">
+      <p>Reduksi
+        <input name="persen" type="text" id="persen" size="10" />
+        Persen </p>
+        <p align="center">
+          <input type="button" name="Button" value="Hitung" class="hitung_button"/>   
+        </p>
+    </form></td>
+    <td width="13%" rowspan="3" class="table_cell1">&nbsp;</td>
+    <td width="10%" rowspan="3" class="table_cell1">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="27" class="table_cell1">Stan Kini</td>
+    <td class="table_cell1">:</td>
+  </tr>
+  <tr>
+    <td height="27" class="table_cell1">Pemakaian</td>
+    <td class="table_cell1">:</td>
+  </tr>
+  <tr>
+    <td height="34" class="table_cell1">Uang Air</td>
+    <td class="table_cell1">:</td>
+    <td width="16%" class="table_cell1">Uang Air &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;:</td>
+    <td width="12%" class="table_cell1 right">&nbsp;</td>
+    <td class="table_cell1">Uang Air &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;:</td>
+    <td class="table_cell1 right">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="27" class="table_cell1">NILAI TOTAL </td>
+    <td class="table_cell1">:</td>
+    <td class="table_cell1">NILAI TOTAL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;     &nbsp;&nbsp;&nbsp;&nbsp;</td>
+    <td class="table_cell1 right">&nbsp;</td>
+    <td class="table_cell1">NILAI TOTAL &nbsp;&nbsp;&nbsp;&nbsp;:</td>
+    <td class="table_cell1 right">&nbsp;</td>
+  </tr>
+  <tr bgcolor="#02153F" class="table_validator">
+    <td height="30" colspan="8"><div align="right"><input name="Submit" type="submit" value="Reduksi" />
+       <input name="Submit2" type="submit" value="Batal" />
     </div></td>
-  </tr>";
-echo"</table>";
-echo"</fieldset>";
-			
+  </tr>
+</table>
+</fieldset>
+<?			
 break;
 		default:
 			$data1[]	= array("rek_bln"=>"1","bln_nama"=>"Januari");
@@ -140,8 +141,6 @@ break;
 .style1 {font-weight: bold}
 -->
 </style>
-
-
 
 <h2><?php echo _NAME; ?></h2><hr/>
 <input type="hidden" class="cekDSR" name="appl_kode" 	value="<?php echo _KODE; ?>"/>
